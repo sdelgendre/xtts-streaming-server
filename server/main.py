f73757054a168f0bb84a3bc181638bf81aa6e6da
QuentinDiago <quentin@diago.ai>
2024-07-15 15:52:50 +0200

    Changed voice dir path

--- server/main.py
+++ server/main.py
@@ -188,7 +188,7 @@ def predict_streaming_generator(parsed_input: dict = Body(...), ulaw : bool = Tr
     voice_path = os.path.join('voices',voice_id+'.json')
     if not os.path.exists(voice_path):
         print("Speaker file not found, using default voice")
-        voice_path = 'voices/default_speaker.json'
+        voice_path = './voices/default_speaker.json'
 
     with open(voice_path,'r') as speaker_file:
         speaker = json.load(speaker_file)
@@ -262,7 +262,7 @@ def predict_speech(parsed_input: TTSInputs):
     voice_path = os.path.join('voices',voice_id+'.json')
     if not os.path.exists(voice_path):
         print("Speaker file not found, using default voice")
-        voice_path = 'voices/default_speaker.json'
+        voice_path = './voices/default_speaker.json'
     
     with open(voice_path,'r') as speaker_file:
         speaker = json.load(speaker_file)
