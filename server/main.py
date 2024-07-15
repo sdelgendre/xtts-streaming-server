8d56af79da55ef842b7fdbddaa6598404ba8c296
QuentinDiago <quentin@diago.ai>
2024-07-15 17:36:17 +0200

    changed header method

--- server/main.py
+++ server/main.py
@@ -216,7 +216,8 @@ def predict_streaming_generator(parsed_input: dict = Body(...), ulaw : bool = Tr
         # Création du header si on est au premier chunk
         if chunk is not None:
             if add_wav_header:
-                yield encode_audio_common(chunk, encode_base64=False)
+                chunk_with_header = encode_audio_common(chunk, encode_base64=False)
+                yield chunk_with_header
             else:
                 yield chunk.tobytes()
 
